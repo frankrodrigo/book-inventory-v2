f2f06f930cfc6092b7a0c01e78d197a4ebe11277
frank <frank.chirapa@gmail.com>
2024-10-24 22:17:45 -0400

    Default IP app and environmental variable for flask APP

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api', // Flask API base URL
+  baseURL: process.env.VUE_APP_BACKEND_URL || 'http://localhost:5000/api', // Use environment variable or fallback to localhost
   withCredentials: false,
   headers: {
     Accept: 'application/json',
